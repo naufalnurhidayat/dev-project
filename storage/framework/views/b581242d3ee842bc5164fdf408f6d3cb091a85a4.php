d3989484fee9191b9d8bc8b3f3c685e2fa95d00e
Naufal Nur Hidayat <naufal.gen13@gmail.com>
2020-02-20 10:04:29 +0700

    Mengupdate beberapa file

--- storage/framework/views/b581242d3ee842bc5164fdf408f6d3cb091a85a4.php
+++ storage/framework/views/b581242d3ee842bc5164fdf408f6d3cb091a85a4.php
@@ -107,6 +107,12 @@
   <!-- Page level custom scripts -->
   <script src="<?php echo e(asset('sbadmin2')); ?>/js/demo/chart-area-demo.js"></script>
   <script src="<?php echo e(asset('sbadmin2')); ?>/js/demo/chart-pie-demo.js"></script>
+  <!-- Page level plugins -->
+  <script src="<?php echo e(asset('sbadmin2')); ?>/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="<?php echo e(asset('sbadmin2')); ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="<?php echo e(asset('sbadmin2')); ?>/js/demo/datatables-demo.js"></script>
 
   <!-- Page level plugins -->
   <script src="<?php echo e(asset('sbadmin2')); ?>/vendor/datatables/jquery.dataTables.min.js"></script>
