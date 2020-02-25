86dbff55faf9214c1c4535b9e242ab0c7da49b98
Tyo <tyo32@gmail.com>
2020-02-25 14:30:17 +0700

    Bugfix and fitur

--- app/Role.php
+++ app/Role.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Stream extends Model
+class Role extends Model
 {
     protected $table = 'role';
     protected $fillable = ['role'];
