d08396eb45c6a8bd33e11c73926bd9fff19055dc
iqbalmaulana <iqbalmaulana162002@gmail.com>
2020-02-25 13:52:11 +0700

    membuat crud stream

---
+++ resources/views/admin/stream/create.blade.php
@@ -0,0 +1,25 @@
+@extends('templates/template-admin')
+
+@section('title', 'Tambah Stream')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h3>Form Tambah Stream</h3>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-4">
+				<form method="post" action="{{url('/admin/stream')}}">
+					@csrf
+					<div class="form-group">
+						<label for="stream">Stream</label>
+						<input type="text" class="form-control @error('stream') is-invalid @enderror" id="stream" name="stream" value="{{ old('stream') }}">
+						@error('stream') <div class="invalid-feedback">{{ $message }}</div> @enderror
+						<button type="submit" class="btn btn-primary mt-3">Tambah</button>
+					</form>
+			</div>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/admin/stream/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('templates/template-admin')
+
+@section('title', 'Edit Stream')
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<h3><i class="fas fa-edit"></i> Edit Stream</h3>
+		</div>
+	</div>
+  <div class="row">
+		<div class="col-4">
+			<form method="post" action="{{'/admin/stream'}}/{{ $stream->id }}">
+				@method('patch')
+				@csrf
+				<div class="form-group">
+					<label for="stream">Stream</label>
+					<input type="text" class="form-control @error('stream') is-invalid @enderror" id="stream" name="stream" value="{{ $stream->stream }}">
+					@error('stream') <div class="invalid-feedback">{{ $message }}</div> @enderror
+				<button type="submit" class="btn btn-primary mt-3">Edit</button>
+			</form>
+		</div>
+  </div>
+</div>
+
+@endsection
