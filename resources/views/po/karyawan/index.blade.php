bb6cf4f960b270de72d6ffc1cd67580cd1059275
Naufal Nur Hidayat <naufal.gen13@gmail.com>
2020-03-04 09:46:03 +0700

    bugfix

--- resources/views/po/karyawan/index.blade.php
+++ resources/views/po/karyawan/index.blade.php
@@ -44,7 +44,7 @@
                       <td>{{ $k->Projek->project }}</td>
                       <td>
                         <a href="{{ url('/po/karyawan/' . $k->User['id']) }}" class="btn btn-primary btn-sm"><i class="fa fa-search-plus"></i> Detail</a>
-                        <form action="{{ url('/admin/karyawan') }}/{{$k->id}}" method="POST" class="d-inline">
+                        <form action="{{ url('/admin/karyawan/' . $k->id)}}" method="POST" class="d-inline">
                           @method('delete')
                           @csrf
                           <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Yakin?')"><i class="fa fa-trash"></i> Hapus</button>
